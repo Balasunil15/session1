d9ac75ea8a16fb6e40156a8812d16f09e0bf09a3
BALASUNIL P <sunildeva2007@gmail.com>
2025-04-12 16:21:33 +0530

    best

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
-app.get('/test', (req, res) => {
-    res.send('Hello World!')
-  })
+
 const port = 3000
 
 async function getProducts (id){
@@ -22,9 +20,4 @@ app.get('/products/:id', async (req, res) => {
     console.log(req.params.id);
     const products = await getProducts(req.params.id);
     res.send(products);
-})
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
